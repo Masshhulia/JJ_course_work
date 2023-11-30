509b85a5f8559310553ba9dcf5a222a3aa823d4d
Maria <mashkabasharim@gmail.com>
2023-11-30 20:49:17 +0300

    feat: add tests router

--- routes/TestsRouter.js
+++ routes/TestsRouter.js
@@ -1,11 +1,10 @@
+// Ваш роутер для тестов (testsRouter.js)
 const Router = require('express');
+const testsController = require('../controllers/testsController');
 
 const router = new Router();
 
-
-router.post('/',)
-router.get('/',)
-
-
+router.get('/tests', testsController.getAllTests);
+router.get('/tests/:id', testsController.getTestById);
 
 module.exports = router;
