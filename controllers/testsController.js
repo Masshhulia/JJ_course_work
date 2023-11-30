fe734b609228e757aed054d727f2777f43101ae0
Maria <mashkabasharim@gmail.com>
2023-11-30 20:53:15 +0300

    feat: add tests controller

--- controllers/testsController.js
+++ controllers/testsController.js
@@ -1,15 +1,33 @@
-class TestsController{
-    async registration(req, res){
+const { Tests } = require('../models/models');
+const ApiError = require('../error/ApiError');
 
+class TestsController {
+    async getAllTests(req, res, next) {
+        try {
+            const tests = await Tests.findAll();
+            return res.json(tests);
+        } catch (error) {
+            console.error('Error fetching tests:', error);
+            next(ApiError.internal('Error fetching tests from the database'));
+        }
     }
 
-    async login(req, res){
-        
+    async getTestById(req, res, next) {
+        try {
+            const testId = req.params.id;
+            const test = await Tests.findByPk(testId);
+
+            if (!test) {
+                return next(ApiError.notFound('Test not found'));
+            }
+
+            return res.json(test);
+        } catch (error) {
+            next(ApiError.internal('Internal Server Error'));
+        }
     }
 
-    async check(req, res){
-        
-    }
+    // Дополнительные методы, если нужны
 }
 
-module.exports = new TestsController()
+module.exports = new TestsController();
