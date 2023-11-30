285aa9c3a0d6bbece89d9cb4fcd8172b3faab4f8
Maria <mashkabasharim@gmail.com>
2023-11-30 20:48:57 +0300

    feat: add Questions router

--- routes/QuestionsRouter.js
+++ routes/QuestionsRouter.js
@@ -1,11 +1,10 @@
 const Router = require('express');
+const questionsController = require('../controllers/questionsController');
 
 const router = new Router();
 
-
-router.post('/',)
-router.get('/',)
-
+router.get('/questions', questionsController.getAllQuestions);
+router.get('/questions/:id', questionsController.getQuestionById);
 
 
 module.exports = router;
