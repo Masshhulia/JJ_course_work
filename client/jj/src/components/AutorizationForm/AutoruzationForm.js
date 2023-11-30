04aa95fdbadd86908e02b602b10641c735271c87
Maria <mashkabasharim@gmail.com>
2023-11-30 20:59:28 +0300

    feat: add change structure of page

--- client/jj/src/components/AutorizationForm/AutoruzationForm.js
+++ client/jj/src/components/AutorizationForm/AutoruzationForm.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { login } from '../../http/userApi';
+import { useNavigate } from 'react-router-dom';
 
 const AuthorizationForm = () => {
-  const handleSubmit = (e) => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e) => setEmail(e.target.value);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Добавьте здесь логику обработки отправки формы
+
+    try {
+      const userData = await login(email, password);
+      console.log('Пользователь вошел:', userData);
+      navigate('/account');
+      // По желанию, вы можете перенаправить на другую страницу или выполнить другие действия после успешной авторизации
+    } catch (error) {
+      console.error('Ошибка авторизации:', error.message);
+      // Добавьте вывод ошибки на экран или другие действия при неудачной авторизации
+    }
   };
 
   return (
@@ -14,9 +32,11 @@ const AuthorizationForm = () => {
         </div>
         <div className="form__container">
           <form onSubmit={handleSubmit} className="form">
-            <input type="text" className="input_field" placeholder="Login/Email" />
-            <input type="password" className="input_field" placeholder="Password" />
-            <button type="submit" className="submit__button2">Submit</button>
+            <input type="text" className="input_field" placeholder="Login/Email" onChange={handleEmailChange} />
+            <input type="password" className="input_field" placeholder="Password" onChange={handlePasswordChange} />
+            <button type="submit" className="submit__button2">
+              Submit
+            </button>
           </form>
         </div>
       </div>
@@ -25,3 +45,4 @@ const AuthorizationForm = () => {
 };
 
 export default AuthorizationForm;
+
