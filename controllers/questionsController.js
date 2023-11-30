b75228c79db2bc9351e49a82a1b1179c564031a8
Maria <mashkabasharim@gmail.com>
2023-11-30 20:54:05 +0300

    feat: add questions controller

--- controllers/questionsController.js
+++ controllers/questionsController.js
@@ -1,15 +1,36 @@
-class QuestionsController{
-    async registration(req, res){
+const { Tests, Questions, Options } = require('../models/models');
+const ApiError = require('../error/ApiError');
 
+class QuizController {
+    async getAllQuestions(req, res, next) {
+        try {
+            const questions = await Questions.findAll({
+                include: [Options],
+            });
+            return res.json(questions);
+        } catch (error) {
+            console.error('Error fetching questions:', error);
+            next(ApiError.internal('Error fetching questions from the database'));
+        }
     }
 
-    async login(req, res){
-        
+    async getQuestionById(req, res, next) {
+        try {
+            const questionId = req.params.id;
+            const question = await Questions.findByPk(questionId, {
+                include: [{ model: Options, as: 'options' }],
+            });
+
+            if (!question) {
+                return next(ApiError.notFound('Question not found'));
+            }
+
+            return res.json(question);
+        } catch (error) {
+            next(ApiError.internal('Internal Server Error'));
+        }
     }
 
-    async check(req, res){
-        
-    }
 }
 
-module.exports = new QuestionsController()
+module.exports = new QuizController();
