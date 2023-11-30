1f5fad3f3cc0b4207102c3b6144c9daf3550766c
Maria <mashkabasharim@gmail.com>
2023-11-30 20:53:48 +0300

    feat: add roadmaps controller

--- controllers/roadmapsController.js
+++ controllers/roadmapsController.js
@@ -1,18 +1,15 @@
-const {Roadmaps} = require('../models/models')
-const ApiError = require('../error/ApiError')
-
-
-
 class RoadmapsController{
-    async create(req, res){
-        const {title} = req.body
-        const roadmap = await Roadmaps.create({title})
-        return res.json(roadmap)
-    }
-    async getAll(req, res){
-      const roadmaps = await Roadmaps.findAll()
-      return res.json(roadmaps)  
-    }
+  async registration(req, res){
+
+  }
+
+  async login(req, res){
+      
+  }
+
+  async check(req, res){
+      
+  }
 }
 
 module.exports = new RoadmapsController()
