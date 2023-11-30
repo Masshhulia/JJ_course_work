8ce9622e8fcac95cc75d793fbfc23921d5427e1e
Maria <mashkabasharim@gmail.com>
2023-11-30 20:49:37 +0300

    feat: add roadmaps router

--- routes/RoadmapsRouter.js
+++ routes/RoadmapsRouter.js
@@ -1,10 +1,19 @@
+// const Router = require('express');
+// const router = new Router();
+// const roadmapsController = require('../controllers/roadmapsController.js'); // Обновлено имя контроллера
+// const authMiddleware = require('../middleware/authMiddleware.js');
+
+// router.get('/frontend', authMiddleware, roadmapsController.getAllRoadmaps);
+
+
+// module.exports = router;
 const Router = require('express');
 
 const router = new Router();
-const roadmapsController = require('../controllers/roadmapsController')
 
-router.post('/',roadmapsController.create)
-router.get('/',roadmapsController.getAll)
+
+router.post('/',)
+router.get('/',)
 
 
 
