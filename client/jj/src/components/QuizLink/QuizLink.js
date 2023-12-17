3d3abdc5e5296940f548db62f77dca4958b61697
Maria <mashkabasharim@gmail.com>
2023-12-17 20:34:04 +0300

    fix: add new logs

--- client/jj/src/components/QuizLink/QuizLink.js
+++ client/jj/src/components/QuizLink/QuizLink.js
@@ -2,10 +2,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const QuizLink = ({ to, text, description }) => {
+const QuizLink = ({ to, text, description, testId }) => {
+  const handleQuizLinkClick = () => {
+    console.log('QuizLink Clicked', testId);
+    localStorage.setItem('selectedTestId', testId);
+    const storedTestId = localStorage.getItem('selectedTestId');
+  console.log('Stored TestId:', storedTestId);
+  };
+
   return (
     <div className="container1">
-      <Link to={to} className="quiz__link">
+      <Link to={to} className="quiz__link" onClick={handleQuizLinkClick}>
         <p className="text_in_button-r">{text}</p>
         <p className="text_in_button2-r">{description}</p>
       </Link>
@@ -13,4 +20,6 @@ const QuizLink = ({ to, text, description }) => {
   );
 };
 
+
 export default QuizLink;
+
