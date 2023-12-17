a01129c98079a30782b50aaec3f07db64d5c7acc
Maria <mashkabasharim@gmail.com>
2023-12-17 20:20:19 +0300

    feat: add new ways

--- routes/AccountRouter.js
+++ routes/AccountRouter.js
@@ -5,5 +5,9 @@ const authMiddleware = require('../middleware/authMiddleware');
 const accountController = require('../controllers/AccountController');
 
 router.get('/info', authMiddleware, accountController.getAccountInfo);
+router.get('/rating', authMiddleware, accountController.getAllUsers)
+router.put('/info', authMiddleware, accountController.updateAccountInfo);
+
+
 
 module.exports = router;
