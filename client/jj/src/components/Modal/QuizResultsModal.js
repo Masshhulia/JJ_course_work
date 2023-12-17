827dbdb8908902bcc3a565003a5531bebb3c8ebc
Maria <mashkabasharim@gmail.com>
2023-12-17 20:44:15 +0300

    feat: add quiz results modal

--- client/jj/src/components/Modal/QuizResultsModal.js
+++ client/jj/src/components/Modal/QuizResultsModal.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import '../../../src/style.css'
 const QuizResultsModal = ({ results, onClose }) => {
-    // Реализуйте пользовательский интерфейс модального окна с использованием результатов
+    
     console.log('Rendering QuizResultsModal')
     return (
-      <div className="modal" style={{ zIndex: 99999 }}> {/* Увеличьте z-index, чтобы модальное окно отображалось поверх других элементов */}
+      <div className="modal" style={{ zIndex: 99999 }}> 
         <div className="modal-content">
           <h2>Результаты квиза</h2>
-          {/* Отобразите результаты квиза здесь */}
           <button onClick={onClose}>Закрыть</button>
         </div>
       </div>
