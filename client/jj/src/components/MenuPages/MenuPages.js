7f7a1c2e56c3a99dfa8f36dc3e5ab7c46642a3e0
Maria <mashkabasharim@gmail.com>
2023-12-17 20:41:24 +0300

    feat: add menu pages component

---
+++ client/jj/src/components/MenuPages/MenuPages.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const MenuPages = () => {
+  return (
+    <div className="menu_pages">
+      <a href="#" className="page_item">RoadMaps</a>
+      <div className="line-list acc"></div>
+      <a href="#" className="page_item">Rating</a>
+      <div className="line-list acc"></div>
+      <a href="#" className="page_item">Calendar</a>
+      <div className="line-list acc"></div>
+      <a href="#" className="page_item">Tests</a>
+      <div className="line-list acc"></div>
+    </div>
+  );
+};
+
+export default MenuPages;
