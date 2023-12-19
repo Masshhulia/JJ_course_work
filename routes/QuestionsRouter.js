4053d0239f8fa8815e1d0375b250a4129a2387df
Maria <mashkabasharim@gmail.com>
2023-12-19 19:51:08 +0300

    feat: add admin rout

--- routes/QuestionsRouter.js
+++ routes/QuestionsRouter.js
@@ -1,10 +1,12 @@
 const Router = require('express');
 const questionsController = require('../controllers/questionsController');
-
+const checkRoleMiddleware = require('../middleware/checkRoleMiddleware')
+const authMiddleware = require('../middleware/authMiddleware')
 const router = new Router();
 
 router.get('/questions', questionsController.getAllQuestions);
-router.get('/questions/:id', questionsController.getQuestionById);
+router.get('/questions/:id',authMiddleware,checkRoleMiddleware('ADMIN'), questionsController.getQuestionById);
+router.post('/questions',authMiddleware,checkRoleMiddleware('ADMIN'), questionsController.createQuestion);
 
 
 module.exports = router;
