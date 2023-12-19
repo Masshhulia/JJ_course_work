83d21034ba38b0c419cc3c6dc29e6f239f89e945
Maria <mashkabasharim@gmail.com>
2023-12-19 19:50:51 +0300

    feat: add admin rout

--- routes/RMLinksRouter.js
+++ routes/RMLinksRouter.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const linksController = require('../controllers/RMLinksController');
+const checkRoleMiddleware = require('../middleware/checkRoleMiddleware')
+const authMiddleware = require('../middleware/authMiddleware')
 
 router.get('/links', linksController.getAllLinks);
 router.get('/links/page/:pageID', linksController.getLinksForPage); 
 router.get('/links/:id', linksController.getLinkById);
+
+router.post('/links',authMiddleware, checkRoleMiddleware('ADMIN') , linksController.createLink); 
 
 module.exports = router;
