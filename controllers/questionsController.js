0347f35ef737287dd354e3499a69c4340b434db7
Maria <mashkabasharim@gmail.com>
2023-12-19 19:54:21 +0300

    feat: add func for admin

--- controllers/questionsController.js
+++ controllers/questionsController.js
@@ -16,21 +16,39 @@ class QuizController {
 
     async getQuestionById(req, res, next) {
         try {
-            const questionId = req.params.id;
-            const question = await Questions.findByPk(questionId, {
-                include: [{ model: Options, as: 'options' }],
-            });
-
-            if (!question) {
-                return next(ApiError.notFound('Question not found'));
+          const questionId = req.params.id;
+          const question = await Questions.findByPk(questionId, {
+            include: [{ model: Options, as: 'options' }],
+          });
+      
+          if (!question) {
+            return next(ApiError.notFound('Question not found'));
+          }
+      
+          return res.json(question);
+        } catch (error) {
+          
+          next(error);
+        }
+      }
+      async createQuestion(req, res, next) {
+        try {
+            const { question_text } = req.body;
+    
+            if (!question_text) {
+                return next(ApiError.badRequest('Question text is required'));
             }
-
+    
+            const question = await Questions.create({ question_text });
+    
             return res.json(question);
         } catch (error) {
-            next(ApiError.internal('Internal Server Error'));
+            console.error('Error creating question:', error);
+            next(ApiError.internal('Error creating question in the database'));
         }
     }
+    
 
-}
+};
 
 module.exports = new QuizController();
