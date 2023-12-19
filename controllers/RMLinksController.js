5055502f0baef9e2cf1e010d3b42020dde4879b9
Maria <mashkabasharim@gmail.com>
2023-12-19 19:54:10 +0300

    feat: add func for admin

--- controllers/RMLinksController.js
+++ controllers/RMLinksController.js
@@ -47,6 +47,32 @@ class LinksController {
       next(ApiError.internal('Internal Server Error'));
     }
   }
+
+
+  async createLink(req, res, next) {
+    try {
+        let { pages_ID, link_title, url } = req.body; 
+        pages_ID = Number(pages_ID);
+        if (pages_ID === '' || pages_ID === null || pages_ID === undefined) {
+          return next(ApiError.badRequest('pages_ID must not be empty'));
+      }
+      pages_ID = Number(pages_ID);
+      if (!Number.isInteger(pages_ID)) {
+          return next(ApiError.badRequest('pages_ID must be an integer'));
+      }
+        const page = await RoadmapsPages.findByPk(pages_ID);
+        
+        if (!page) {
+            return next(ApiError.badRequest('Page does not exist'));
+        }
+        const link = await RoadmapsLinks.create({ pages_ID, link_title, url });
+        return res.json(link);
+    } catch (error) {
+        console.error('Error creating link:', error);
+        next(ApiError.internal('Error creating link in the database'));
+    }
+}
+
 }
 
 module.exports = new LinksController();
