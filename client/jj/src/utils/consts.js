a8d56afdea1e79eb0cb2ee61fc83012a1045ac0a
Maria <mashkabasharim@gmail.com>
2023-12-19 19:55:46 +0300

    feat: add admin rout

--- client/jj/src/utils/consts.js
+++ client/jj/src/utils/consts.js
@@ -1,4 +1,4 @@
-// export const ADMIN_ROUTE = '/admin'
+export const ADMIN_ROUTE = '/admin'
 export const LOGIN_ROUTE = '/login'
 export const AUTORIZE_ROUTE = '/auth'
 export const REGISTRATION_ROUTE = '/registration'
