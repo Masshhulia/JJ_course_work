478b7e9c631035f33f8e5d48e3ad07ce502c6320
Maria <mashkabasharim@gmail.com>
2023-12-19 19:50:36 +0300

    feat: add admin rout

--- routes/TestsRouter.js
+++ routes/TestsRouter.js
@@ -1,10 +1,13 @@
-// Ваш роутер для тестов (testsRouter.js)
+
 const Router = require('express');
 const testsController = require('../controllers/testsController');
+const checkRoleMiddleware = require('../middleware/checkRoleMiddleware')
+const authMiddleware = require('../middleware/authMiddleware')
 
 const router = new Router();
 
 router.get('/tests', testsController.getAllTests);
 router.get('/tests/:id', testsController.getTestById);
+router.post('/tests', authMiddleware, checkRoleMiddleware('ADMIN'), testsController.createTest);
 
 module.exports = router;
