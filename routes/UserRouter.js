9070ade71a2781020e83dc65af313aa2a0614165
Maria <mashkabasharim@gmail.com>
2023-12-19 19:49:15 +0300

    feat: add admin rout

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -2,11 +2,15 @@ const Router = require('express');
 const router = new Router();
 const userController = require('../controllers/userController.js')
 const authMiddleware = require('../middleware/authMiddleware.js')
+const checkRoleMiddleware = require('../middleware/checkRoleMiddleware.js')
 
 
 router.post('/registration', userController.registration)
 router.post('/login', userController.login )
 router.get('/check', authMiddleware, userController.check)
+router.get('/', authMiddleware, checkRoleMiddleware('ADMIN'), userController.getAll)
+
+
 
 
 module.exports = router;
