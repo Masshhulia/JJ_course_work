86710305c63e22fb982e3be5551c5c249fbe5dab
Maria <mashkabasharim@gmail.com>
2025-04-09 13:21:49 +0300

    2

--- client/jj/src/components/TestResultsProgress/TestResultsProgress.js
+++ client/jj/src/components/TestResultsProgress/TestResultsProgress.js
@@ -1,25 +1,26 @@
+import React from 'react';
+
 const TestResults = ({ testResults }) => {
     return (
-      <div>
-        {Array.isArray(testResults) ? (
-          testResults.map((result, index) => (
-            <div key={index} className="test-result">
-              <h2 style={{ color: '#1DC9A0' }}>{result.title}</h2>
-              <div className="w3-light-grey">
-                <div
-                  className="w3-container w3-green"
-                  style={{ height: '10px', width: `${result.score}%` }}
-                ></div>
-              </div>
-              <div style={{ fontSize: '20px' }}>{result.score} из 100%</div>
-            </div>
-          ))
-        ) : (
-          <div>No test results available.</div>
-        )}
-      </div>
+        <div>
+            {Array.isArray(testResults) && testResults.length > 0 ? (
+                testResults.map((result) => (
+                    <div key={result.testId} className="test-result">
+                        <h2 style={{ color: '#1DC9A0' }}>{result.title}</h2>
+                        <div className="w3-light-grey">
+                            <div
+                                className="w3-container w3-green"
+                                style={{ height: '10px', width: `${result.score}%` }}
+                            ></div>
+                        </div>
+                        <div style={{ fontSize: '20px' }}>{result.score} из 100%</div>
+                    </div>
+                ))
+            ) : (
+                <div>No test results available.</div>
+            )}
+        </div>
     );
-  };
-  
-  
-  export default TestResults;
+};
+
+export default TestResults;
